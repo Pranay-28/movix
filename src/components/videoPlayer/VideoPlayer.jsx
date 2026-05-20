5df32614f977781c1677a1277d4198f463a7b938
Pranay-28 <pranaybambole11@gmail.com>
2026-05-20 11:49:02 +0530

    fix

--- src/components/videoPlayer/VideoPlayer.jsx
+++ src/components/videoPlayer/VideoPlayer.jsx
@@ -379,9 +379,8 @@ const VideoPlayer = ({ mediaType, tmdbId }) => {
                                         height="500"
                                         frameBorder="0"
                                         allowFullScreen
-                                        allow="autoplay; encrypted-media; fullscreen"
+                                        allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
                                         referrerPolicy="no-referrer"
-                                        sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
                                         style={{ pointerEvents: "auto", zIndex: 10 }}
                                         onError={handleSourceError}
                                         onLoad={handleIframeLoad}
